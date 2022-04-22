8c7d3b01890a00b12de333f170b6680ce2dcc04c
Shingo <kawanos@google.com>
2022-04-22 21:44:37 +0900

    change the role of the file

--- datagetter/db.py
+++ datagetter/db.py
@@ -5,31 +5,48 @@ import decimal
 import json
 import logging
 import time
+from uuid import uuid4
 
 from google.cloud import spanner
 from google.cloud.spanner_v1 import param_types
 
 OPERATION_TIMEOUT_SECONDS = 240
 
-class Datagetter:
+class MySpanner:
+
     def __init__(self, instance_id, database_id):
-        self.instance_id = instance_id
-        self.database_id = database_id
         spanner_client = spanner.Client()
         instance = spanner_client.instance(instance_id)
         self.database = instance.database(database_id)
 
 
-    def query_data(self, ):
-        """Queries sample data from the database using SQL."""
-        spanner_client = spanner.Client()
-        instance = spanner_client.instance(instance_id)
-        database = instance.database(database_id)
-
-        with database.snapshot() as snapshot:
+    def query_data(self, name: str) -> any:
+        with self.database.snapshot() as snapshot:
             results = snapshot.execute_sql(
-                "SELECT SingerId, AlbumId, AlbumTitle FROM Albums"
+                "SELECT id,name,age from test where name = @name",
+                params={"name": name},
+                param_types={"name": spanner.param_types.STRING}
             )
+        return results
 
-            for row in results:
-                print(u"SingerId: {}, AlbumId: {}, AlbumTitle: {}".format(*row))
+    def insert_with_dml(self, name: str, age: int) -> None: 
+
+        def insert_singers(transaction):
+            id = str(uuid4())
+            row_ct = transaction.execute_update(
+                "INSERT test (id, name, age) VALUES (@id, @name, @age)",
+                params={"id":id, "name":name, "age":age},
+                param_types={"name": spanner.param_types.STRING, "id": spanner.param_types.STRING, "age": spanner.param_types.INT64}
+            )
+            print("{} record(s) inserted.".format(row_ct))
+
+        self.database.run_in_transaction(insert_singers)
+
+if __name__ == '__main__':
+    import sys
+    instance_id, database_id = sys.argv[1:3]
+    print(instance_id, database_id)
+    s = MySpanner(instance_id, database_id)
+    s.insert_with_dml("foo", 100)
+    for v in s.query_data("foo"):
+        print(v)
