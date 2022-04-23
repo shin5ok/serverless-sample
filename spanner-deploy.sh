0915407e1fa2ee89cb6b9273e684e599c3c92e85
shin5ok <shin5ok@55mp.com>
2022-04-23 14:03:14 +0900

    change the field

--- spanner-deploy.sh
+++ spanner-deploy.sh
@@ -5,6 +5,6 @@ cmd="spanner-cli -i test-instance -d testdb -p $PROJECT"
 
 cat <<'EOD' | $cmd
 
-CREATE TABLE test (id STRING(36) NOT NULL,name STRING(64), age INT64) PRIMARY KEY(id);
+CREATE TABLE test (id STRING(36) NOT NULL,name STRING(64), score INT64) PRIMARY KEY(id);
 
 EOD
