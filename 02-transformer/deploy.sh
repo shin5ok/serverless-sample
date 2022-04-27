ddf1b6e223d8324ddf84fb823d4171f3460584a6
Shingo <kawanos@google.com>
2022-04-27 15:54:04 +0900

    set BUCKET_NAME env to $PROJECT value

--- 02-transformer/deploy.sh
+++ 02-transformer/deploy.sh
@@ -1,1 +1,2 @@
+BUCKET_NAME=$PROJECT
 gcloud run deploy --source=. --set-env-vars=INSTANCE_ID=$INSTANCE_ID,DATABASE_ID=$DATABASE_ID,BUCKET_NAME=$BUCKET_NAME --region=us-central1 transformer
--- 03-requester/deploy.sh
+++ 03-requester/deploy.sh
@@ -1,1 +1,2 @@
+BUCKET_NAME=$PROJECT
 gcloud run deploy --source=. --set-env-vars=BUCKET_NAME=$BUCKET_NAME,SLACK_API=$SLACK_API --region=us-central1 requester
