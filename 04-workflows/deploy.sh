fd8e70bf5f6b0eddb1c9ec6e1e5424eab89c5e36
shin5ok <shin5ok@55mp.com>
2022-05-20 18:05:57 +0900

    add location arg

--- 04-workflows/deploy.sh
+++ 04-workflows/deploy.sh
@@ -4,5 +4,6 @@ envsubst < templates/workflows.yaml > /tmp/workflows.yaml
 SERVICE_ACCOUNT=workflows
 gcloud workflows deploy workflow \
 --source=/tmp/workflows.yaml \
+--location=us-central1 \
 --service-account=${SERVICE_ACCOUNT}@$PROJECT.iam.gserviceaccount.com
 rm -f /tmp/workflow.yaml
