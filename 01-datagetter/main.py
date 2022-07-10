b90f1a0632c2720b05f1db3ae8508cdb13de8f3b
shin5ok <shin5ok@55mp.com>
2022-07-10 14:13:53 +0900

    remove old codes

--- 01-datagetter/main.py
+++ 01-datagetter/main.py
@@ -25,24 +25,5 @@ if __name__ == '__main__':
     }
     uvicorn.run("main:app", **options)
 
-#@app.route("/test")
-#def _test() -> Any:
-#    return f"{os.environ.get('K_SERVICE', 'local')} ok\n", 200
-#
-#@app.route("/api/<string:name>/<int:score>", methods=["POST"])
-#def _pathinfo(name: str, score: int) -> Any:
-#    s = MySpanner(INSTANCE_ID, DATABASE_ID)
-#    id: str = s.insert_with_dml(name, score)
-#    return jsonify({"name":name, "id": id}), 200
-#
-#@app.route("/api/gen")
-#def _main() -> Any:
-#    response = requests.get(EXTERNAL_API)
-#    data: dict = json.loads(response.content)[0]
-#    s = MySpanner(INSTANCE_ID, DATABASE_ID)
-#    id: str = s.insert_with_dml(data['name'], data['score'])
-#    return jsonify({"name":data['name'], "id": id}), 200
-
-
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
